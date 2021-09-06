c42e76c5bb06caf95eb9823262f8fc7816d1784e
ArielOrdenes <arieloordenes@live.com>
2021-09-06 18:34:21 -0300

    fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :post_find, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order('created_at DESC')
   end
@@ -19,16 +21,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(params[:post].permit(:title, :body))
       redirect_to @post
     else
@@ -37,13 +35,15 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
-
     redirect_to root_path
   end
 
   private
+
+  def post_find
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :body)
